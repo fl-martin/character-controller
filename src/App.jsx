95d3ea9d0d51e6259ca2ecd24ef0abab83c90dc6
Federico L. Martin <fl.martin.dev@gmail.com>
2024-05-25 18:02:16 -0300

    Overlay components

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,18 @@ import { Canvas } from "@react-three/fiber";
 import CharacterControllerDemo from "./components/canvas/CharacterControllerDemo";
 import { AdaptiveDpr } from "@react-three/drei";
 import { Suspense } from "react";
-import LoadingScreen from "./components/canvas/LoadingScreen";
+import LoadingScreen from "./components/dom/LoadingScreen";
 import Overlay from "./components/dom/Overlay";
 
 function App() {
 	return (
-		<>
+		<Suspense fallback={<LoadingScreen />}>
 			<Canvas performance={{ min: 0.5, max: 1 }} shadows>
-				<Suspense fallback={<LoadingScreen />}>
-					<AdaptiveDpr />
-					<CharacterControllerDemo />
-				</Suspense>
+				<AdaptiveDpr />
+				<CharacterControllerDemo />
 			</Canvas>
 			<Overlay />
-		</>
+		</Suspense>
 	);
 }
 
--- src/components/dom/Overlay.jsx
+++ src/components/dom/Overlay.jsx
@@ -1,38 +1,46 @@
-const Overlay = () => {
-	return (
-		<div className="absolute bottom-5 left-1/2 -translate-x-1/2 flex gap-3 items-center opacity-60">
-			<div className="flex flex-col items-center gap-1">
-				<kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
-					W
-				</kbd>
-				<div className="flex gap-1">
-					<kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
-						A
-					</kbd>
-					<kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
-						S
-					</kbd>
-					<kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
-						D
-					</kbd>
-				</div>
-			</div>
+import { useProgress } from "@react-three/drei";
 
-			<div className="flex flex-col gap-2">
-				<div className="flex gap-1 items-center">
-					<kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
-						Shift
-					</kbd>
-					<span className="text-white font-sans ">Run</span>
+const Overlay = () => {
+	const { active } = useProgress();
+
+	return (
+		<>
+			{!active && (
+				<div className="absolute bottom-5 left-1/2 -translate-x-1/2 flex gap-3 items-center opacity-60">
+					<div className="flex flex-col items-center gap-1">
+						<kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
+							W
+						</kbd>
+						<div className="flex gap-1">
+							<kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
+								A
+							</kbd>
+							<kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
+								S
+							</kbd>
+							<kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
+								D
+							</kbd>
+						</div>
+					</div>
+
+					<div className="flex flex-col gap-2">
+						<div className="flex gap-1 items-center">
+							<kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
+								Shift
+							</kbd>
+							<span className="text-white font-sans ">Run</span>
+						</div>
+						<div className="flex gap-1 items-center">
+							<kbd className="px-4 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
+								Spacebar
+							</kbd>
+							<span className="text-white font-sans ">Jump</span>
+						</div>
+					</div>
 				</div>
-				<div className="flex gap-1 items-center">
-					<kbd className="px-4 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
-						Spacebar
-					</kbd>
-					<span className="text-white font-sans ">Jump</span>
-				</div>
-			</div>
-		</div>
+			)}
+		</>
 	);
 };
 

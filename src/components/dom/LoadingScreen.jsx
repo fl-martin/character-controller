90bc7c0943d3dd0eeec695fd44034119fc63e234
Federico L. Martin <fl.martin.dev@gmail.com>
2024-05-27 10:23:15 -0300

    README clear

--- src/components/dom/LoadingScreen.jsx
+++ src/components/dom/LoadingScreen.jsx
@@ -1,4 +1,4 @@
-import { Html, useProgress } from "@react-three/drei";
+import { useProgress } from "@react-three/drei";
 
 export default function LoadingScreen() {
 	const { progress } = useProgress();
